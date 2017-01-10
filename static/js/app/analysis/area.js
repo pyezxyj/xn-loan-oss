d9de64d3ed3f66a8b4831a54f4fd91dd5bb3d323
Administrator <Administrator@MS-20160709RIHI>
2017-01-10 15:21:18 +0800

    upload

--- static/js/app/analysis/area.js
+++ static/js/app/analysis/area.js
@@ -13,9 +13,13 @@ $(function() {
     }, {
         field: 'time',
         title: '平均耗时（时）',
-        formatter: function(v) {
-           return ((+v/3600).toFixed(2));
-        },
+        formatter: function (v) {
+            if ( v != null ) {
+                     return ((+v/3600000).toFixed(2)); 
+            } else{
+            	return  '-' ;
+            }
+        }
     },{
         field: 'money',
         title: '垫资总量',
--- static/js/app/analysis/node.js
+++ static/js/app/analysis/node.js
@@ -8,8 +8,12 @@ $(function() {
     }, {
         field: 'average',
         title: '平均耗时（时）',
-        formatter: function(v) {
-            return ((+v/3600).toFixed(2));
+        formatter: function (v) {
+            if ( v != null ) {
+                     return ((+v/3600000).toFixed(2)); 
+            } else{
+            	return  '-' ;
+            }
         }
     }];
 
